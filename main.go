357bbcee8698a0050e52fe8b131db89db1cebc33
seb07 <wild.sebastian@outlook.de>
2023-07-10 10:59:10 +0200

    initial

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 
 	provider "github.com/seb07-cloud/cdktf-vm-win-linux/generated/azurerm/provider"
@@ -12,15 +13,22 @@ func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
 	// New Azurerm Provider instance
-	azurermProvider := provider.NewAzurermProvider(stack, "azurerm", &provider.AzurermProviderConfig{
+	provider.NewAzurermProvider(stack, "azurerm", &provider.AzurermProviderConfig{
 		Features: &[]*provider.AzurermProviderFeatures{},
 	})
 
 	// New Resource Group instance
 	resGrp := resourceGroup.NewResourceGroup(stack, "resourceGroup", &provider.ResourceGroupConfig{
-		Name:     &"cdktf-vm-win-linux",
-		Location: &"westeurope",
+		Name:     jsii.String("cdktf-vm-win-linux"),
+		Location: jsii.String("westeurope"),
 	})
+
+	// output the resource group name
+	cdktf.NewTerraformOutput(stack, jsii.String("names"), &cdktf.TerraformOutputConfig{
+		Value: &[]*string{resGrp.Name()},
+	})
+
+	return stack
 }
 
 func main() {
