a481bc6b6b57431e8bc3c1b0b3e1683f961d634b
seb07 <wild.sebastian@outlook.de>
2023-07-11 18:07:03 +0200

    added vm config

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 
 const (
 	// Azure location to deploy the resources to
-	azureLocation    = "westeurope"
-	vnetAddressSpace = "10.0.0.0/16"
-	snetAddressSpace = "10.0.0.0/24"
+	resourceGroupName = "rg-cdktf-vm-win-linux"
+	azureLocation     = "westeurope"
+	vnetName          = "vnet-cdktf-vm-win-linux"
+	vnetAddressSpace  = "10.0.0.0/16"
+	snetAddressSpace  = "10.0.0.0/24"
 
 	// Azure Virtual Machine based consts
 	vmName          = "cdktf-vm-win-linux"
@@ -19,6 +21,13 @@ const (
 	vmOsType        = "Linux"
 	vmAdminUsername = "cdktfuser"
 	vmAdminPassword = "cdktfpassword"
+
+	// Azure Storage based consts
+	storageOsDisk = "cdktfosdisk"
+	publisher     = "Canonical"
+	offer         = "UbuntuServer"
+	sku           = "18.04-LTS"
+	version       = "latest"
 )
 
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
@@ -28,16 +37,53 @@ func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	shared.NewAzProvider(stack, "azurerm")
 
 	// Create a resource group
-	rgrp := shared.NewAzResourceGroupConfig("cdktf-vm-win-linux", azureLocation)
+	rgrp := shared.NewAzResourceGroupConfig(resourceGroupName, azureLocation)
 	shared.CreateAzResourceGroup(stack, *rgrp)
 
 	// Create a virtual network
-	vnet := shared.NewAzVnetConfig("cdktf-vm-win-linux", vnetAddressSpace, azureLocation, rgrp.Name, snetAddressSpace)
+	vnet := shared.NewAzVnetConfig(vnetName, vnetAddressSpace, azureLocation, rgrp.Name, snetAddressSpace)
 	shared.CreateAzVirtualNetwork(stack, *vnet)
 
 	// Create a vm with a public ip
-	vm := shared.NewAzVirtualMachineConfig(vmName, azureLocation, rgrp.Name, vmSize, 30, vmOsType, vmAdminUsername, vmAdminPassword, vnet, vnet.Subnet)
-	shared.CreateAzVirtualMachine(stack, *vm)
+	nicconfig := shared.NewAzNetworkInterfaceConfig(vmName, azureLocation, rgrp.Name, vnet, vnet.Subnet)
+	nicId := shared.CreateAzNetworkInterface(stack, *nicconfig)
+
+	// Create Storage Image Reference
+	vmir := &shared.VirtualMachineStorageImageReferenceConfig{
+		Publisher: publisher,
+		Offer:     offer,
+		Sku:       sku,
+		Version:   version,
+	}
+
+	// Create Storage Os Disk Config
+	vmosd := &shared.VirtualMachineStorageOsDiskConfig{
+		Name:            storageOsDisk,
+		Caching:         "ReadWrite",
+		CreateOption:    "FromImage",
+		DiskSizeGb:      vmOsDiskSizeGb,
+		ManagedDiskType: "Standard_LRS",
+	}
+
+	// Create a virtual machine config
+	vmconfig := &shared.VirtualMachineConfig{
+		Name:                        vmName,
+		Location:                    azureLocation,
+		ResourceGroupName:           rgrp.Name,
+		VmSize:                      vmSize,
+		OsDiskSizeGb:                vmOsDiskSizeGb,
+		OsType:                      vmOsType,
+		AdminUsername:               vmAdminUsername,
+		AdminPassword:               vmAdminPassword,
+		Vnet:                        vnet,
+		Subnet:                      vnet.Subnet,
+		NicId:                       nicId,
+		StorageOsDiskConfig:         vmosd,
+		StorageImageReferenceConfig: vmir,
+	}
+
+	// Create a virtual machine
+	shared.CreateAzVirtualMachine(stack, *vmconfig)
 
 	return stack
 }
--- shared/vm.go
+++ shared/vm.go
@@ -1,24 +1,103 @@
 package shared
 
+import (
+	"fmt"
+
+	"github.com/aws/jsii-runtime-go"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+
+	networkinterface "cdk.tf/go/stack/generated/azurerm/networkinterface"
+	virtualmachine "cdk.tf/go/stack/generated/azurerm/virtualmachine"
+)
+
 // a function to create a virtual machine and return a pointer to it.
 
 // new struct for virtual machine config
-
 type VirtualMachineConfig struct {
+	Name                        string
+	Location                    string
+	ResourceGroupName           string
+	VmSize                      string
+	OsDiskSizeGb                int
+	OsType                      string
+	AdminUsername               string
+	AdminPassword               string
+	Vnet                        *VnetConfig
+	Subnet                      *SubnetConfig
+	NicId                       string
+	StorageOsDiskConfig         *VirtualMachineStorageOsDiskConfig
+	StorageImageReferenceConfig *VirtualMachineStorageImageReferenceConfig
+}
+
+// a struct for network interface config
+type NetworkInterfaceConfig struct {
 	Name              string
 	Location          string
 	ResourceGroupName string
-	VmSize            string
-	OsDiskSizeGb      int
-	OsType            string
-	AdminUsername     string
-	AdminPassword     string
 	Vnet              *VnetConfig
 	Subnet            *SubnetConfig
 }
 
+// virtual machine storage os disk struct
+type VirtualMachineStorageOsDiskConfig struct {
+	Name            string
+	Caching         string
+	CreateOption    string
+	DiskSizeGb      int
+	ManagedDiskType string
+}
+
+// Storage Image Reference struct
+type VirtualMachineStorageImageReferenceConfig struct {
+	Publisher string
+	Offer     string
+	Sku       string
+	Version   string
+}
+
+// a function to initialize a VirtualMachineConfig struct
+func (vmc *VirtualMachineConfig) Init(name string, location string, resourceGroupName string, vmSize string, osDiskSizeGb int, osType string, adminUsername string, adminPassword string, vnet *VnetConfig, subnet *SubnetConfig, nicId string) {
+	vmc.Name = name
+	vmc.Location = location
+	vmc.ResourceGroupName = resourceGroupName
+	vmc.VmSize = vmSize
+	vmc.OsDiskSizeGb = osDiskSizeGb
+	vmc.OsType = osType
+	vmc.AdminUsername = adminUsername
+	vmc.AdminPassword = adminPassword
+	vmc.Vnet = vnet
+	vmc.Subnet = subnet
+	vmc.NicId = nicId
+}
+
+// a function to initialize a NetworkInterfaceConfig struct
+func (nic *NetworkInterfaceConfig) Init(name string, location string, resourceGroupName string, vnet *VnetConfig, subnet *SubnetConfig) {
+	nic.Name = name
+	nic.Location = location
+	nic.ResourceGroupName = resourceGroupName
+	nic.Vnet = vnet
+	nic.Subnet = subnet
+}
+
+// a function to initialize a VirtualMachineStorageOsDisk struct
+func (vmos *VirtualMachineStorageOsDiskConfig) Init(name string, caching string, createOption string, diskSizeGb int, managedDiskType string) {
+	vmos.Name = name
+	vmos.Caching = caching
+	vmos.CreateOption = createOption
+	vmos.DiskSizeGb = diskSizeGb
+	vmos.ManagedDiskType = managedDiskType
+}
+
+// a function to initialize a VirtualMachineStorageImageReference struct
+func (vmir *VirtualMachineStorageImageReferenceConfig) Init(publisher string, offer string, sku string, version string) {
+	vmir.Publisher = publisher
+	vmir.Offer = offer
+	vmir.Sku = sku
+	vmir.Version = version
+}
+
 // a function to create a virtualmachine.VirtualMachineConfig and return a pointer to it.
-func NewAzVirtualMachineConfig(name string, location string, resourceGroupName string, vmSize string, osDiskSizeGb int, osType string, adminUsername string, adminPassword string, vnet *VnetConfig, subnet *SubnetConfig) *VirtualMachineConfig {
+func NewAzVirtualMachineConfig(name string, location string, resourceGroupName string, vmSize string, osDiskSizeGb int, osType string, adminUsername string, adminPassword string, vnet *VnetConfig, subnet *SubnetConfig, nicId string) *VirtualMachineConfig {
 
 	return &VirtualMachineConfig{
 		Name:              name,
@@ -31,80 +110,72 @@ func NewAzVirtualMachineConfig(name string, location string, resourceGroupName s
 		AdminPassword:     adminPassword,
 		Vnet:              vnet,
 		Subnet:            subnet,
+		NicId:             nicId,
 	}
+}
+
+// a function to create a network interface and return the id
+func NewAzNetworkInterfaceConfig(name string, location string, resourceGroupName string, vnet *VnetConfig, subnet *SubnetConfig) *NetworkInterfaceConfig {
+
+	return &NetworkInterfaceConfig{
+		Name:              name,
+		Location:          location,
+		ResourceGroupName: resourceGroupName,
+		Vnet:              vnet,
+		Subnet:            subnet,
+	}
+}
+
+// a function which takes a network interface config and creates a virtualmachine.NetworkInterface and returns the id
+func CreateAzNetworkInterface(stack cdktf.TerraformStack, nic NetworkInterfaceConfig) string {
+	nicID := networkinterface.NewNetworkInterface(stack, jsii.String("networkinterface"),
+		&networkinterface.NetworkInterfaceConfig{
+			Name:              jsii.String(nic.Name),
+			Location:          jsii.String(nic.Location),
+			ResourceGroupName: jsii.String(nic.ResourceGroupName),
+			IpConfiguration: []*networkinterface.NetworkInterfaceIpConfiguration{
+				{
+					Name:                       jsii.String(nic.Name),
+					PrivateIpAddressAllocation: jsii.String("Dynamic"),
+					Primary:                    jsii.Bool(true),
+				},
+			},
+		},
+	)
+	n := fmt.Sprintf("%v", nicID.Id())
+	return n
 }
 
 // a function which takes a virtual machine config and creates a virtualmachine.VirtualMachine
 func CreateAzVirtualMachine(stack cdktf.TerraformStack, vm VirtualMachineConfig) virtualmachine.VirtualMachine {
+
+	// convert networkinterface.NetworkInterface to *[]*string
+	nicIdSlice := StringSlice(vm.NicId)
+
 	return virtualmachine.NewVirtualMachine(stack, jsii.String("virtualmachine"),
 		&virtualmachine.VirtualMachineConfig{
 			Name:              jsii.String(vm.Name),
 			Location:          jsii.String(vm.Location),
 			ResourceGroupName: jsii.String(vm.ResourceGroupName),
 			VmSize:            jsii.String(vm.VmSize),
-			OsDisk: []*virtualmachine.VirtualMachineOsDisk{
-				{
-					Name: jsii.String("osdisk"),
-					Caching: func() *string {
-						caching := "ReadWrite"
-						return &caching
-					}(),
-					CreateOption: func() *string {
-						createOption := "FromImage"
-						return &createOption
-					}(),
-					DiskSizeGb: jsii.Number(vm.OsDiskSizeGb),
-					ManagedDiskType: func() *string {
-						managedDiskType := "Standard_LRS"
-						return &managedDiskType
-					}(),
-					OsType: jsii.String(vm.OsType),
-				},
+			OsProfile: &virtualmachine.VirtualMachineOsProfile{
+				ComputerName:  jsii.String(vm.Name),
+				AdminUsername: jsii.String(vm.AdminUsername),
+				AdminPassword: jsii.String(vm.AdminPassword),
 			},
-			StorageImageReference: []*virtualmachine.VirtualMachineStorageImageReference{
-				{
-					Id: jsii.String("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/terraform-vm-win-linux/providers/Microsoft.Compute/images/terraform-vm-win-linux-image"),
-				},
+			NetworkInterfaceIds: nicIdSlice,
+			StorageOsDisk: &virtualmachine.VirtualMachineStorageOsDisk{
+				Name:            jsii.String(vm.StorageOsDiskConfig.Name),
+				Caching:         jsii.String(vm.StorageOsDiskConfig.Caching),
+				CreateOption:    jsii.String(vm.StorageOsDiskConfig.CreateOption),
+				DiskSizeGb:      jsii.Number(vm.StorageOsDiskConfig.DiskSizeGb),
+				ManagedDiskType: jsii.String(vm.StorageOsDiskConfig.ManagedDiskType),
 			},
-			StorageOsDisk: []*virtualmachine.VirtualMachineStorageOsDisk{
-				{
-					Name: jsii.String("osdisk"),
-					Caching: func() *string {
-						caching := "ReadWrite"
-						return &caching
-					}(),
-					CreateOption: func() *string {
-						createOption := "FromImage"
-						return &createOption
-					}(),
-					DiskSizeGb: jsii.Number(vm.OsDiskSizeGb),
-					ManagedDiskType: func() *string {
-						managedDiskType := "Standard_LRS"
-						return &managedDiskType
-					}
-				},
-			},
-			OsProfile: []*virtualmachine.VirtualMachineOsProfile{
-				{
-					AdminUsername: jsii.String(vm.AdminUsername),
-					AdminPassword: jsii.String(vm.AdminPassword),
-					ComputerName:  jsii.String(vm.Name),
-				},
-			},
-			NetworkInterface: []*virtualmachine.VirtualMachineNetworkInterface{
-				{
-					Name: jsii.String("nic"),
-					IpConfiguration: []*virtualmachine.VirtualMachineNetworkInterfaceIpConfiguration{
-						{
-							Name: jsii.String("ipconfig"),
-							Primary: func() *bool {
-								primary := true
-								return &primary
-							},
-							SubnetId: jsii.String(vm.Subnet.Id),
-						},
-					},
-				},
+			StorageImageReference: &virtualmachine.VirtualMachineStorageImageReference{
+				Publisher: jsii.String(vm.StorageImageReferenceConfig.Publisher),
+				Offer:     jsii.String(vm.StorageImageReferenceConfig.Offer),
+				Sku:       jsii.String(vm.StorageImageReferenceConfig.Sku),
+				Version:   jsii.String(vm.StorageImageReferenceConfig.Version),
 			},
 		},
 	)
